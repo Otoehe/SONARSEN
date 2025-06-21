31242f4049b5919b7387f280898809c890eca8ee
BMB <victorsonarsen@gmail.com>
2025-06-21 22:46:28 +0300

    Update main.jsx
    app on map

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './Map/Map'; // Імпортуємо компонент карти
+import Map from './Map/Map'; // Імпортуємо компонент карти
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
